cdba4e600926ecc195f73a405d952e14ad0e649e
Lenny Werrn <lenny.werrn@energie360.ch>
2024-06-26 13:02:42 +0200

    Working on object.js

--- src/objects/objects.spec.js
+++ src/objects/objects.spec.js
@@ -8,8 +8,8 @@ import {
     removeProperty,
     createPeople,
     updateAge,
-    addMethod
-} from './index'
+    addMethod,
+} from './index';
 
 const person = {
     name: 'Lenny',
@@ -19,19 +19,19 @@ const person = {
 
 
 describe('Objects exercises', () => {
-    it.todo('should return the list of all properties of an object', () => {
+    it('should return the list of all properties of an object', () => {
         expect(getProperties(person)).toEqual(['name', 'age', 'city'])
     })
 
-    it.todo('should return the list of all values of an object', () => {
+    it('should return the list of all values of an object', () => {
         expect(getValues(person)).toEqual(['Lenny', 20, 'Zürich'])
     })
 
-    it.todo('should return the list of all entries of an object', () => {
+    it('should return the list of all entries of an object', () => {
         expect(getEntries(person)).toEqual([['name', 'Lenny'], ['age', 20], ['city', 'Zürich']])
     })
 
-    it.todo('should create a new object based on person, with the same properties and values but with a new property', () => {
+    it('should create a new object based on person, with the same properties and values but with a new property', () => {
         const copy = addProperty(person, 'country', 'Switzerland')
         expect(copy).toEqual({
             name: 'Lenny',
@@ -42,11 +42,11 @@ describe('Objects exercises', () => {
         expect(copy).not.toBe(person)
     })
 
-    it.todo('should return the number of properties of an object', () => {
+    it('should return the number of properties of an object', () => {
         expect(countProperties(person)).toBe(3)
     })
 
-    it.todo('should remove a property from an object', () => {
+    it('should remove a property from an object', () => {
         const copy = addProperty(person, 'country', 'Switzerland')
         removeProperty(copy, 'age')
 
@@ -61,18 +61,17 @@ describe('Objects exercises', () => {
         expect(countProperties(person)).toBe(2)
     })
 
-    it.todo('should create an array of objects based on the given object', () => {
-        const people = createPeople(person, 3)
+    it('should create an array of objects based on the given object', () => {
+        const people = createPeople(person, 3);
         expect(people).toEqual([
             { name: 'Lenny', age: 20, city: 'Zürich' },
             { name: 'Lenny', age: 20, city: 'Zürich' },
             { name: 'Lenny', age: 20, city: 'Zürich' }
-        ])
-    })
+        ]);
+    });
 
-    it.todo('should update the age of a person', () => {
-        // Enter your age here`
-        const newAge = null
+    it('should update the age of a person', () => {
+        const newAge = 18;
         const updatedPerson = updateAge(person, newAge)
         expect(updatedPerson).toEqual({
             name: 'Lenny',
@@ -82,11 +81,12 @@ describe('Objects exercises', () => {
         expect(typeof updatedPerson.age).toBe('number')
     })
 
-    it.todo('should add a method to the person object', () => {
-        // Enter the method implementation here
-        const method = null
-        addMethod(person, 'greet', method)
+    it('should add a method to the person object', () => {
+        const method = function() {
+            return `Hello, my name is ${this.name}`;
+        };
+        addMethod(person, 'greet', method);
 
-        expect(person.greet()).toBe('Hello, my name is Lenny')
-    })
+        expect(person.greet()).toBe('Hello, my name is Lenny');
+    });
 })
